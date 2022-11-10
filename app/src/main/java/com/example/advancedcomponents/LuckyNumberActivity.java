eb6b8d8b5a077b373ef93039bd2bbc649836d4a8
Sumanth krishna M <sumanth.krishna@improve10x.com>
2022-11-10 15:47:44 +0530

    added a deleteButton and written its functionality

--- app/src/main/java/com/example/advancedcomponents/LuckyNumberActivity.java
+++ app/src/main/java/com/example/advancedcomponents/LuckyNumberActivity.java
@@ -6,30 +6,51 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class LuckyNumberActivity extends AppCompatActivity {
 
-    public Integer[] luckyNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public  ArrayList<Integer> luckyNumbers;
+     public Spinner luckyNumbersSp;
+     public ArrayAdapter<Integer> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lucky_number);
         getSupportActionBar().setTitle("Lucky Number");
+        setUpLuckyNumbersData();
         setUpLuckyNumbers();
     }
 
+    public void setUpLuckyNumbersData() {
+       luckyNumbers= new  ArrayList<Integer>();
+       luckyNumbers.add(1);
+       luckyNumbers.add(2);
+       luckyNumbers.add(3);
+       luckyNumbers.add(4);
+       luckyNumbers.add(5);
+       luckyNumbers.add(6);
+       luckyNumbers.add(7);
+       luckyNumbers.add(8);
+       luckyNumbers.add(9);
+       luckyNumbers.add(10);
+    }
+
     public void setUpLuckyNumbers() {
-        Spinner luckyNumbersSp = findViewById(R.id.lucky_numbers_sp);
-        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this, R.layout.dropdownitem, luckyNumbers);
+        luckyNumbersSp = findViewById(R.id.lucky_numbers_sp);
+        arrayAdapter = new ArrayAdapter<Integer>(this, R.layout.dropdownitem, luckyNumbers);
         luckyNumbersSp.setAdapter(arrayAdapter);
         luckyNumbersSp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(LuckyNumberActivity.this, "Lucky Number : " + luckyNumbers[position], Toast.LENGTH_SHORT).show();
+                Toast.makeText(LuckyNumberActivity.this, "Lucky Number : " + luckyNumbers, Toast.LENGTH_SHORT).show();
             }
 
             @Override
--- app/src/main/java/com/example/advancedcomponents/TeamListActivity.java
+++ app/src/main/java/com/example/advancedcomponents/TeamListActivity.java
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -23,8 +24,20 @@ public class TeamListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_team_list);
         getSupportActionBar().setTitle("Team List");
         handleAddButton();
+        handleDeleteButton();
         setUpData();
-        teamMember();
+        teamMembersList();
+    }
+
+    public void handleDeleteButton() {
+        Button deleteBtn = findViewById(R.id.delete_btn);
+        deleteBtn.setOnClickListener(view -> {
+            EditText memberNameTxt = findViewById(R.id.member_name_txt);
+            String memberName = memberNameTxt.getText().toString();
+            arrayAdapter.remove(memberName);
+            arrayAdapter.notifyDataSetChanged();
+            memberNameTxt.setText("");
+        });
     }
 
     public void handleAddButton() {
@@ -32,24 +45,29 @@ public class TeamListActivity extends AppCompatActivity {
         addBtn.setOnClickListener(view -> {
             EditText memberNameTxt = findViewById(R.id.member_name_txt);
             String memberName = memberNameTxt.getText().toString();
-            if(memberName.equals("")== false){
+            if (memberName.equals("") == false) {
                 arrayAdapter.add(memberName);
                 arrayAdapter.notifyDataSetChanged();
             }
             memberNameTxt.setText("");
         });
     }
-
-
+    
     public void setUpData() {
         namesList = new ArrayList<>();
         namesList.add("Viswanath");
         namesList.add("Renuka");
     }
 
-    public void teamMember() {
+    public void teamMembersList() {
         teamListLv = findViewById(R.id.team_list_lv);
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, namesList);
         teamListLv.setAdapter(arrayAdapter);
+        teamListLv.setOnItemClickListener((adapterView, view, position, l) -> {
+            Toast.makeText(this, "Clicked : " + namesList.get(position), Toast.LENGTH_SHORT).show();
+            String memberName = namesList.get(position);
+            EditText memberNameTxt = findViewById(R.id.member_name_txt);
+            memberNameTxt.setText(memberName);
+        });
     }
 }
